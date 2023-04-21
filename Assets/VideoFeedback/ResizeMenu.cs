6a6f6872aecf24d9d929f4662b3de4939b9e20ab
Andrew Dawson <dawso.andrew@gmail.com>
2023-04-21 13:47:54 -0400

    Fixes the menu size not being reset when another player respawns the menus.

--- Assets/VideoFeedback/ResizeMenu.cs
+++ Assets/VideoFeedback/ResizeMenu.cs
@@ -3,10 +3,16 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
 
 public class ResizeMenu : UdonSharpBehaviour
 {
     public void OnRespawn()
+    {
+        SendCustomNetworkEvent(NetworkEventTarget.All, "OnRespawnNetwork");
+    }
+
+    public void OnRespawnNetwork()
     {
         transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
     }
