1a713881b457fea0aa99f6a21cdc99a6e9f00e53
Andrew Dawson <dawso.andrew@gmail.com>
2023-04-23 17:55:42 -0400

    Adds post processing and floor level settings.

---
+++ Assets/VideoFeedback/FloorLevel.cs
@@ -0,0 +1,23 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FloorLevel : UdonSharpBehaviour
+{
+    public Slider slider;
+
+    private BoxCollider boxCollider;
+
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    public void OnChangeFloorLevel()
+    {
+        boxCollider.size = new Vector3(boxCollider.size.x, 2.0f * slider.value, boxCollider.size.z);
+    }
+}
---
+++ Assets/VideoFeedback/PostProcessingSettings.cs
@@ -0,0 +1,59 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class PostProcessingSettings : UdonSharpBehaviour
+{
+    public Slider ambientOcclusionIntensitySlider;
+    public PostProcessVolume ambientOcclusionVolume;
+    public Slider bloomIntensitySlider;
+    public PostProcessVolume bloomVolume;
+    public CanvasGroup postProcessCanvasGroup;
+    public Toggle postProcessingToggle;
+
+    private PostProcessVolume postProcessVolume;
+
+    void Start()
+    {
+        postProcessVolume = GetComponent<PostProcessVolume>();
+
+        bloomIntensitySlider.SetValueWithoutNotify(bloomVolume.weight);
+        ambientOcclusionIntensitySlider.SetValueWithoutNotify(ambientOcclusionVolume.weight);
+    }
+
+    public void OnChangeAmbientOcclusionIntensity()
+    {
+        ambientOcclusionVolume.weight = ambientOcclusionIntensitySlider.value;
+    }
+
+    public void OnChangeBloomIntensity()
+    {
+        bloomVolume.weight = bloomIntensitySlider.value;
+    }
+
+    public void OnChangePostProcessing()
+    {
+        var isEnabled = postProcessingToggle.isOn;
+
+        postProcessVolume.enabled = isEnabled;
+        bloomVolume.enabled = isEnabled;
+        ambientOcclusionVolume.enabled = isEnabled;
+
+        if (isEnabled)
+        {
+            postProcessCanvasGroup.alpha = 1.0f;
+            postProcessCanvasGroup.blocksRaycasts = true;
+            postProcessCanvasGroup.interactable = true;
+        }
+        else
+        {
+            postProcessCanvasGroup.alpha = 0.1f;
+            postProcessCanvasGroup.blocksRaycasts = false;
+            postProcessCanvasGroup.interactable = false;
+        }
+    }
+}
