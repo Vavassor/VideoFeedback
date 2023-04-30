17e7aa9eca5d97f4487a3e3f5b2ada1889119adc
Andrew Dawson <dawso.andrew@gmail.com>
2023-04-30 05:45:39 -0400

    Fixes field of view being enabled when in orthographic projection. Also fixes menu colliders.

---
+++ Assets/VideoFeedback/ProjectionSettings.cs
@@ -0,0 +1,38 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ProjectionSettings : UdonSharpBehaviour
+{
+    public CameraController cameraController;
+    public SyncedToggle orthographicProjectionToggle;
+
+    private CanvasGroup canvasGroup;
+
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnChangeOrthographicProjection()
+    {
+        var isEnabled = !orthographicProjectionToggle.isOn;
+
+        if (isEnabled)
+        {
+            canvasGroup.alpha = 1.0f;
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.interactable = true;
+        }
+        else
+        {
+            canvasGroup.alpha = 0.25f;
+            canvasGroup.blocksRaycasts = false;
+            canvasGroup.interactable = false;
+        }
+
+        cameraController.OnChangeOrthographicProjection();
+    }
+}
