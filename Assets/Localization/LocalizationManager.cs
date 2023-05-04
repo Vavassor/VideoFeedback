61a2078ae8bbbabe0e2b80c1d00eac6b9abfe513
Andrew Dawson <dawso.andrew@gmail.com>
2023-05-04 17:46:00 -0400

    Makes all text localizable and updates the pseudolocale.

--- Assets/Localization/LocalizationManager.cs
+++ Assets/Localization/LocalizationManager.cs
@@ -1,4 +1,5 @@
-﻿using UdonSharp;
+﻿using System.Globalization;
+using UdonSharp;
 using UnityEngine;
 using VRC.SDK3.Data;
 using VRC.SDKBase;
@@ -41,19 +42,24 @@ public class LocalizationManager : UdonSharpBehaviour
         foreach (var text in texts)
         {
             var foundValue = FindValue(text.key, locale);
+
             if (foundValue == null)
             {
                 foundValue = FindValue(text.key, defaultLocale);
             }
+
             if (foundValue == null)
             {
                 foundValue = placeholderValue;
             }
-            text.SetText(foundValue);
+
+            var interpolatedValue = Interpolate(text, foundValue);
+            var transformedValue = Transform(text, interpolatedValue);
+            text.SetText(transformedValue);
         }
     }
 
-    void Start()
+    private void Start()
     {
         LoadResources();
         SetLocale(defaultLocale);
@@ -109,5 +115,83 @@ public class LocalizationManager : UdonSharpBehaviour
         }
 
         return valueToken.String;
+    }
+
+    private string Interpolate(LocalizedText localizedText, string newText)
+    {
+        if (localizedText.interpolationKeys.Length == 0)
+        {
+            return newText;
+        }
+
+        var result = "";
+
+        for (var i = 0; i < newText.Length; i++)
+        {
+            switch (newText[i])
+            {
+                case '{':
+                {
+                    if (i < newText.Length - 2 && newText[i + 1] == '{')
+                    {
+                        int codeStart = i + 2;
+                        int codeEnd = newText.IndexOf("}}", codeStart);
+                        if (codeEnd == -1)
+                        {
+                            Debug.LogError("Failed to get value. Invalid interpolation code.");
+                            return newText;
+                        }
+
+                        var code = newText.Substring(codeStart, codeEnd - codeStart).Trim();
+                        var key = code;
+
+                        var interpolationKeyIndex = FindInterpolationKey(localizedText.interpolationKeys, key);
+                        if (interpolationKeyIndex == -1)
+                        {
+                            Debug.Log("Failed to get value. Unknown interpolation key \"" + key + "\".");
+                            return newText;
+                        }
+
+                        result += localizedText.interpolationValues[interpolationKeyIndex];
+                        i = codeEnd + 1;
+                    }
+                    else
+                    {
+                        result += newText[i];
+                    }
+                    break;
+                }
+                default:
+                {
+                    result += newText[i];
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int FindInterpolationKey(string[] interpolationKeys, string key)
+    {
+        for (var i = 0; i < interpolationKeys.Length; i++)
+        {
+            if (interpolationKeys[i].Equals(key))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private string Transform(LocalizedText localizedText, string newText)
+    {
+        if (localizedText.shouldTransformUppercase)
+        {
+            // Unfortunately, CultureInfo is not exposed to Udon. So we can't tell it to convert
+            // to uppercase based on locale. Instead, use the invariant locale.
+            return newText.ToUpperInvariant();
+        }
+        return newText;
     }
 }
--- Assets/Localization/LocalizedText.cs
+++ Assets/Localization/LocalizedText.cs
@@ -7,23 +7,33 @@ using VRC.Udon;
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class LocalizedText : UdonSharpBehaviour
 {
+    public string[] interpolationKeys;
+    public string[] interpolationValues;
     public string key;
+    /// <summary>
+    /// Convert all characters to uppercase.
+    /// Similar to the CSS property text-transform.
+    /// </summary>
     public bool shouldTransformUppercase;
 
     private Text textComponent;
+    private bool isInitialized;
 
-    void Start()
+    private void EnsureInitialized()
     {
+        if (isInitialized)
+        {
+            return;
+        }
+
         textComponent = GetComponent<Text>();
+
+        isInitialized = true;
     }
 
     public void SetText(string text)
     {
-        if (shouldTransformUppercase)
-        {
-            text = text.ToUpper();
-        }
-
+        EnsureInitialized();
         textComponent.text = text;
     }
 }
