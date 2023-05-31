70c9c711ef94d3f5c49b115f90676ecd413dfee7
Andrew Dawson <dawso.andrew@gmail.com>
2023-05-31 00:50:56 -0400

    Fixes the resize buttons on the color key settings.

---
+++ Assets/VideoFeedback/Teleport.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Teleport : UdonSharpBehaviour
+{
+    public Transform teleportTarget;
+    public VRCObjectSync objectSync;
+
+    public void OnTeleport()
+    {
+        objectSync.TeleportTo(teleportTarget);
+    }
+}
