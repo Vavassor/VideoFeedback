69883390e2cc4a6a272172f9910de7779b89e912
Andrew Dawson <dawso.andrew@gmail.com>
2023-06-04 16:59:54 -0400

    Updates the videos and adds thumbnails.

---
+++ Assets/VideoFeedback/VideoButton.cs
@@ -0,0 +1,44 @@
+﻿
+using UdonSharp;
+using UdonSharp.Video;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+public class VideoButton : UdonSharpBehaviour
+{
+    public USharpVideoPlayer targetVideoPlayer;
+    public VRCUrl[] playlist = new VRCUrl[0];
+
+    Button button;
+
+    void Start()
+    {
+        button = GetComponentInChildren<Button>();
+        UpdateOwnership();
+
+        targetVideoPlayer.RegisterCallbackReceiver(this);
+    }
+
+    public void OnButtonPress()
+    {
+        targetVideoPlayer.PlayVideo(playlist[0]);
+    }
+
+    public void OnUSharpVideoLockChange()
+    {
+        UpdateOwnership();
+    }
+
+    public void OnUSharpVideoOwnershipChange()
+    {
+        UpdateOwnership();
+    }
+
+    void UpdateOwnership()
+    {
+        button.interactable = targetVideoPlayer.CanControlVideoPlayer();
+    }
+}
