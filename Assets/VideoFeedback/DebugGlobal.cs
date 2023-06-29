49d5d568a487964c91fa65b67c84c47873975a04
Andrew Dawson <dawso.andrew@gmail.com>
2023-06-29 17:15:15 -0400

    Fixes the AudioLink theme color menu disappearing.

---
+++ Assets/VideoFeedback/DebugGlobal.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using System.Globalization;
+
+public class DebugGlobal : UdonSharpBehaviour
+{
+    void Start()
+    {
+        Debug.Log("DebugGlobal - Current Culture: " + CultureInfo.CurrentCulture);
+    }
+}
